9cbb299aca0eb33542640c91cd5fc36ae0474237
vinniemoreira <vinniemoreira94@gmail.com>
2017-10-27 08:31:13 -0300

    Main and Simulator classes modification

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,9 +1,25 @@
 package program;
 
+import engine.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String estimationAlgorithm = args[0];
+		int initialTagsNumber = Integer.parseInt(args[1]);
+		int incrementTagRate = Integer.parseInt(args[2]);
+		int maxTagsNumber = Integer.parseInt(args[3]);
+		int iterationsNumber = Integer.parseInt(args[4]);
+		int initialFrameSize = Integer.parseInt(args[5]);
+		
+		
+		Simulator simulator = new Simulator (estimationAlgorithm, initialTagsNumber, incrementTagRate,
+				maxTagsNumber, iterationsNumber, initialFrameSize);
+		
+		simulator.execute();
+		
 
 	}
 
