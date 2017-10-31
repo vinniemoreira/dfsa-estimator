ea1bec2f7bbf7d38dba067283d6a7b7ff6130998
Vinnie Moreira <vinniemoreira94@gmail.com>
2017-10-31 16:50:04 -0300

    eom lee

--- src/engine/EomLeeEstimator.java
+++ src/engine/EomLeeEstimator.java
@@ -2,10 +2,30 @@ package engine;
 
 public class EomLeeEstimator extends Estimator {
 
+	double factorB, factorY, threshold;
+	
+	public EomLeeEstimator() {
+		// TODO Auto-generated constructor stub
+		
+		factorB = Double.POSITIVE_INFINITY;
+		factorY = 2;
+		threshold = 0.1;
+	}
+	
 	@Override
 	Frame calculateNextFrame(Frame frame) {
 		// TODO Auto-generated method stub
-		int n = 1000;
+		double n = 0;
+		double prevFactorY = 0;
+		while ((factorY - prevFactorY) > threshold) {
+			prevFactor
+			factorB = (frame.frameSize)/((prevFactorY*frame.collisionSlots)+frame.successfullSlots);
+			factorY = 
+		}
+		
+		
+		
+		
 		Frame answer = new Frame(n); 
 		return answer;
 	}
